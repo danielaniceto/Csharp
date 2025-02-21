9ed68107c7ea269a698bf23731ebb983b6fe55dd
danielaniceto <danielgustavots@hotmail.com>
2025-02-20 22:12:20 -0300

    Subindo main

---
+++ Routes/PersonRoute.cs
@@ -0,0 +1,11 @@
+﻿using Person.Models;
+
+namespace Person.Routes;
+
+public static class PersonRoute
+{
+    public static void PersonRoutes(this WebApplication app)
+    {
+        app.MapGet("person", () => new PersonModel(name: "Daniel"));
+    }
+}
